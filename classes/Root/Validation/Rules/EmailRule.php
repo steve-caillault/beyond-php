1ae67ba7b94701783b38fb4618316a0606c9f5b4
Stève Caillault <steve.caillault@gmail.com>
2020-08-27 14:39:29 +0200

    Ajout d'une règle de validation pour les adresses email.

---
+++ classes/Root/Validation/Rules/EmailRule.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Vérification qu'une valeur représente une adresse email
+ */
+
+namespace Root\Validation\Rules;
+
+class EmailRule extends Rule {
+	
+	/**
+	 * Message en cas d'erreur
+	 * @var string
+	 */
+	protected string $_error_message = 'La valeur doit être une adresse email valide.';
+	
+	/********************************************************************************/
+	
+	/* VERIFICATION */
+	
+	/**
+	 * Retourne si la valeur respecte la règle
+	 * @return bool
+	 */
+	public function check() : bool
+	{
+		$value = $this->_getValue();
+		
+		$pattern = strtr('/^:sub(\.:sub)*\@:sub+\.[a-zA-Z]{2,10}$/D', [
+			':sub' => '[^\@\.\ ]+'
+		]);
+		
+		return (preg_match($pattern, $value) === 1);
+	}
+	
+	/********************************************************************************/
+	
+}
